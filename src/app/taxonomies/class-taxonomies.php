a6ef97639a76e5d5324e2ff2bce7a2105e53e4e2
P. E. A. Lutz <pea@misfist.com>
2025-12-02 19:43:13 -0500

    Add topics taxonomy

--- src/app/taxonomies/class-taxonomies.php
+++ src/app/taxonomies/class-taxonomies.php
@@ -8,7 +8,7 @@
 namespace Site_Functionality\App\Taxonomies;
 
 use Site_Functionality\Common\Abstracts\Base;
-use Site_Functionality\App\Taxonomies\Publication_Type;
+use Site_Functionality\App\Taxonomies\Topic;
 
 // Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
@@ -33,7 +33,6 @@ class Taxonomies extends Base {
 	 * @return void
 	 */
 	public function init(): void {
-		// new Publication_Type( $this->settings );
+		new Topic( $this->settings );
 	}
-
 }
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'misfist/site-functionality',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1de9e0d92ce38323673629ac6f1774af046f39b3',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'misfist/site-functionality' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1de9e0d92ce38323673629ac6f1774af046f39b3',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
