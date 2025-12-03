ba4a2162202b102dbe0c8be95e503fd27a741393
P. E. A. Lutz <pea@misfist.com>
2025-12-02 22:24:17 -0500

    Add options page

--- src/app/integrations/publish-to-apple-news/class-publish-to-apple-news.php
+++ src/app/integrations/publish-to-apple-news/class-publish-to-apple-news.php
@@ -32,17 +32,318 @@ class Publish_To_Apple_News extends Base {
 	 * @return void
 	 */
 	public function init(): void {
-		add_filter( 'apple_news_component_text_styles', array( $this, 'component_text_styles' ) );
+		add_action( 'acf/include_fields', array( $this, 'register_settings_fields' ) );
 
-		add_filter( 'apple_news_component_styles', array( $this, 'component_styles' ) );
+		add_action( 'acf/init', array( $this, 'register_options_page' ) );
 
-		add_filter( 'apple_news_component_layouts', array( $this, 'component_layouts' ) );
+		// add_filter( 'apple_news_component_text_styles', array( $this, 'component_text_styles' ) );
 
-		add_filter( 'apple_news_in_article_json', array( $this, 'modify_in_article_component' ) );
+		// add_filter( 'apple_news_component_styles', array( $this, 'component_styles' ) );
 
-		add_filter( 'apple_news_end_of_article_json', array( $this, 'modify_end_of_article_component' ) );
+		// add_filter( 'apple_news_component_layouts', array( $this, 'component_layouts' ) );
+
+		// add_filter( 'apple_news_in_article_json', array( $this, 'modify_in_article_component' ) );
+
+		// add_filter( 'apple_news_end_of_article_json', array( $this, 'modify_end_of_article_component' ) );
 
 		add_filter( 'apple_news_exporter_content_pre', array( $this, 'filter_apple_news_content_pre' ), 10, 2 );
+
+		add_filter( 'acf/get_post_types', array( $this, 'enable_block_post_types' ), 10, 1 );
+	}
+
+	/**
+	 * Modify ACF post_object query to include wp_block.
+	 *
+	 * @param array $post_types
+	 * @return array $post_types
+	 */
+	public function enable_block_post_types( $post_types ) {
+		if ( ! in_array( 'wp_block', $post_types ) ) {
+			$post_types[] = 'wp_block';
+		}
+		return $post_types;
+	}
+
+	/**
+	 * Register the ACF field group for Apple News CTA positions.
+	 *
+	 * @return void
+	 */
+	public function register_settings_fields() {
+		if ( ! function_exists( 'acf_add_local_field_group' ) ) {
+			return;
+		}
+
+		acf_add_local_field_group(
+			array(
+				'key'                   => 'group_apple_news_before',
+				'title'                 => __( 'Before Article', 'site-functionality' ),
+				'fields'                => array(
+					array(
+						'key'               => 'field_before_article_heading',
+						'label'             => __( 'Heading', 'site-functionality' ),
+						'name'              => 'before_article_heading',
+						'type'              => 'text',
+						'instructions'      => '',
+						'required'          => 0,
+						'conditional_logic' => 0,
+						'wrapper'           => array(),
+						'default_value'     => '',
+						'maxlength'         => '',
+						'allow_in_bindings' => 0,
+						'placeholder'       => '',
+						'prepend'           => '',
+						'append'            => '',
+					),
+					array(
+						'key'               => 'field_before_article_text',
+						'label'             => __( 'Content', 'site-functionality' ),
+						'name'              => 'before_article_text',
+						'type'              => 'wysiwyg',
+						'instructions'      => '',
+						'required'          => 0,
+						'conditional_logic' => 0,
+						'wrapper'           => array(),
+						'default_value'     => '',
+						'allow_in_bindings' => 0,
+						'tabs'              => 'all',
+						'toolbar'           => 'full',
+						'media_upload'      => 1,
+						'delay'             => 0,
+					),
+					array(
+						'key'               => 'field_before_article_button_text',
+						'label'             => __( 'Button Text', 'site-functionality' ),
+						'name'              => 'before_article_button_text',
+						'type'              => 'text',
+						'instructions'      => '',
+						'required'          => 0,
+						'conditional_logic' => 0,
+						'wrapper'           => array(),
+						'default_value'     => '',
+						'maxlength'         => '',
+						'allow_in_bindings' => 0,
+						'placeholder'       => '',
+						'prepend'           => '',
+						'append'            => '',
+					),
+					array(
+						'key'               => 'field_before_article_button_url',
+						'label'             => __( 'Button URL', 'site-functionality' ),
+						'name'              => 'before_article_button_url',
+						'type'              => 'url',
+						'instructions'      => '',
+						'required'          => 0,
+						'conditional_logic' => 0,
+						'wrapper'           => array(),
+						'default_value'     => '',
+						'allow_in_bindings' => 0,
+						'placeholder'       => '',
+					),
+				),
+				'location'              => array(
+					array(
+						array(
+							'param'    => 'options_page',
+							'operator' => '==',
+							'value'    => 'component-slots',
+						),
+					),
+				),
+				'menu_order'            => 10,
+				'position'              => 'normal',
+				'style'                 => 'default',
+				'label_placement'       => 'top',
+				'instruction_placement' => 'label',
+				'hide_on_screen'        => '',
+				'active'                => true,
+				'description'           => __( 'Content to display before the article content.', 'site-functionality' ),
+				'show_in_rest'          => 1,
+			)
+		);
+
+		acf_add_local_field_group(
+			array(
+				'key'                   => 'group_apple_news_after',
+				'title'                 => __( 'After Article', 'site-functionality' ),
+				'fields'                => array(
+					array(
+						'key'               => 'field_end_of_article_heading',
+						'label'             => __( 'Heading', 'site-functionality' ),
+						'name'              => 'end_of_article_heading',
+						'type'              => 'text',
+						'instructions'      => '',
+						'required'          => 0,
+						'conditional_logic' => 0,
+						'wrapper'           => array(),
+						'default_value'     => '',
+						'maxlength'         => '',
+						'allow_in_bindings' => 0,
+						'placeholder'       => '',
+						'prepend'           => '',
+						'append'            => '',
+					),
+					array(
+						'key'               => 'field_end_of_article_text',
+						'label'             => __( 'Content', 'site-functionality' ),
+						'name'              => 'end_of_article_text',
+						'type'              => 'wysiwyg',
+						'instructions'      => '',
+						'required'          => 0,
+						'conditional_logic' => 0,
+						'wrapper'           => array(),
+						'default_value'     => '',
+						'allow_in_bindings' => 0,
+						'tabs'              => 'all',
+						'toolbar'           => 'full',
+						'media_upload'      => 1,
+						'delay'             => 0,
+					),
+					array(
+						'key'               => 'field_end_of_article_button_text',
+						'label'             => __( 'Button Text', 'site-functionality' ),
+						'name'              => 'end_of_article_button_text',
+						'type'              => 'text',
+						'instructions'      => '',
+						'required'          => 0,
+						'conditional_logic' => 0,
+						'wrapper'           => array(),
+						'default_value'     => '',
+						'maxlength'         => '',
+						'allow_in_bindings' => 0,
+						'placeholder'       => '',
+						'prepend'           => '',
+						'append'            => '',
+					),
+					array(
+						'key'               => 'field_end_of_article_button_url',
+						'label'             => __( 'Button URL', 'site-functionality' ),
+						'name'              => 'end_of_article_button_url',
+						'type'              => 'url',
+						'instructions'      => '',
+						'required'          => 0,
+						'conditional_logic' => 0,
+						'wrapper'           => array(),
+						'default_value'     => '',
+						'allow_in_bindings' => 0,
+						'placeholder'       => '',
+					),
+				),
+				'location'              => array(
+					array(
+						array(
+							'param'    => 'options_page',
+							'operator' => '==',
+							'value'    => 'component-slots',
+						),
+					),
+				),
+				'menu_order'            => 20,
+				'position'              => 'normal',
+				'style'                 => 'default',
+				'label_placement'       => 'top',
+				'instruction_placement' => 'label',
+				'hide_on_screen'        => '',
+				'active'                => true,
+				'description'           => __( 'Content to display after the article content.', 'site-functionality' ),
+				'show_in_rest'          => 1,
+			)
+		);
+
+		acf_add_local_field_group(
+			array(
+				'key'                   => 'group_apple_news_settings',
+				'title'                 => __( 'Apple News Settings', 'site-functionality' ),
+				'fields'                => array(
+					array(
+						'key'                  => 'field_apple_news_before_content',
+						'label'                => __( 'Before Content', 'site-functionality' ),
+						'name'                 => 'before_content',
+						'type'                 => 'post_object',
+						'instructions'         => __( 'Select blocks to display before article content.', 'site-functionality' ),
+						'required'             => 0,
+						'conditional_logic'    => 0,
+						'wrapper'              => array(
+							'width' => '',
+							'class' => '',
+							'id'    => '',
+						),
+						'post_type'            => array( 'wp_block' ),
+						'return_format'        => 'object',
+						'multiple'             => 1,
+						'allow_null'           => 1,
+						'allow_in_bindings'    => 1,
+						'bidirectional'        => 0,
+						'ui'                   => 1,
+						'bidirectional_target' => array(),
+					),
+					array(
+						'key'                  => 'field_apple_news_after_content',
+						'label'                => __( 'After Content', 'site-functionality' ),
+						'name'                 => 'after_content',
+						'type'                 => 'post_object',
+						'instructions'         => __( 'Select blocks to display after article content.', 'site-functionality' ),
+						'required'             => 0,
+						'conditional_logic'    => 0,
+						'wrapper'              => array(
+							'width' => '',
+							'class' => '',
+							'id'    => '',
+						),
+						'post_type'            => array( 'wp_block' ),
+						'return_format'        => 'object',
+						'multiple'             => 1,
+						'allow_null'           => 1,
+						'allow_in_bindings'    => 1,
+						'bidirectional'        => 0,
+						'ui'                   => 1,
+						'bidirectional_target' => array(),
+					),
+				),
+				'location'              => array(
+					array(
+						array(
+							'param'    => 'options_page',
+							'operator' => '==',
+							'value'    => 'component-slots',
+						),
+					),
+				),
+				'menu_order'            => 0,
+				'position'              => 'normal',
+				'style'                 => 'default',
+				'label_placement'       => 'top',
+				'instruction_placement' => 'label',
+				'hide_on_screen'        => '',
+				'active'                => true,
+				'description'           => '',
+				'show_in_rest'          => 0,
+			)
+		);
+	}
+
+	/**
+	 * Register the ACF options page for Component Settings.
+	 *
+	 * @return void
+	 */
+	public function register_options_page() {
+		if ( ! function_exists( 'acf_add_options_page' ) ) {
+			return;
+		}
+
+		acf_add_options_page(
+			array(
+				'page_title'      => __( 'Component Slots', 'site-functionality' ),
+				'menu_slug'       => 'component-slots',
+				'parent_slug'     => 'apple_news_index',
+				'menu_title'      => __( 'Component Settings', 'site-functionality' ),
+				'position'        => 2,
+				'redirect'        => false,
+				'updated_message' => __( 'Component Settings Updated', 'site-functionality' ),
+				'autoload'        => true,
+			)
+		);
 	}
 
 	/**
@@ -154,9 +455,9 @@ class Publish_To_Apple_News extends Base {
 	 * @return array $component
 	 */
 	public function modify_in_article_component( $component ) {
-		$heading     = get_option( 'in_article_heading', __( '', 'lapp' ) );
-		$text        = get_option( 'in_article_text', __( '', 'lapp' ) );
-		$button_text = get_option( 'in_article_button_text', esc_html__( 'Support Our Work', 'lapp' ) );
+		$heading     = get_option( 'in_article_heading', __( '', 'site-functionality' ) );
+		$text        = get_option( 'in_article_text', __( '', 'site-functionality' ) );
+		$button_text = get_option( 'in_article_button_text', esc_html__( 'Support Our Work', 'site-functionality' ) );
 		$url         = get_option( 'in_article_button_url', esc_url( 'https://givebutter.com/joinlapublicpress' ) );
 
 		$component = array(
@@ -207,9 +508,9 @@ class Publish_To_Apple_News extends Base {
 	 * @return array $component
 	 */
 	public function modify_end_of_article_component( $component ) {
-		$heading     = get_option( 'end_of_article_heading', __( 'Subscribe to LA Public Press', 'lapp' ) );
-		$text        = get_option( 'end_of_article_text', __( 'The best way to keep up with Los Angeles Public Press is to subscribe to our weekly newsletter.', 'lapp' ) );
-		$button_text = get_option( 'end_of_article_button_text', esc_html__( 'Sign Up', 'lapp' ) );
+		$heading     = get_option( 'end_of_article_heading', __( 'Subscribe to LA Public Press', 'site-functionality' ) );
+		$text        = get_option( 'end_of_article_text', __( 'The best way to keep up with Los Angeles Public Press is to subscribe to our weekly newsletter.', 'site-functionality' ) );
+		$button_text = get_option( 'end_of_article_button_text', esc_html__( 'Sign Up', 'site-functionality' ) );
 		$url         = get_option( 'end_of_article_button_url', esc_url( get_home_url( null, '/newsletter/' ) ) );
 
 		$component = array(
@@ -247,10 +548,10 @@ class Publish_To_Apple_News extends Base {
 	}
 
 	/**
-	 * Filter Before 
+	 * Filter Before
 	 *
 	 * @param string $content
-	 * @param int $post_id
+	 * @param int    $post_id
 	 *
 	 * @return void
 	 */
