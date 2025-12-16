48cacf3b9746e34aa27de1f821dfcc9e43220070
P. E. A. Lutz <pea@misfist.com>
2025-12-16 00:06:45 -0500

    Build

--- src/app/blocks/build/index.asset.php
+++ src/app/blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-editor', 'wp-element', 'wp-i18n'), 'version' => 'b91d099ad49dd09fcb17');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-editor', 'wp-element', 'wp-i18n'), 'version' => 'a2db3d96e0d11e5a1640');
