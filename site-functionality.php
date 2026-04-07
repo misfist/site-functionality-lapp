0ae3b77c8c6c4f95c074f782c995a144571436de
P. E. A. Lutz <pea@misfist.com>
2026-04-07 15:11:39 -0400

    1.0.1 - Remove Github Actions

--- site-functionality.php
+++ site-functionality.php
@@ -15,7 +15,7 @@
  * Plugin Name:       Site Functionality
  * Plugin URI:        https://github.com/misfist/site-functionality-lapp
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           1.0.0
+ * Version:           1.0.1
  * Requires PHP:      7.4
  * Author:            Misfist
  * Author URI:        https://pealutz.me
@@ -49,7 +49,7 @@ if ( file_exists( $autoload ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'SITE_FUNCTIONALITY_VERSION', '1.0.0' );
+define( 'SITE_FUNCTIONALITY_VERSION', '1.0.1' );
 define( 'SITE_FUNCTIONALITY_BASENAME', plugin_basename( __FILE__ ) );
 define( 'SITE_FUNCTIONALITY_PATH', plugin_dir_path( __FILE__ ) );
 define( 'SITE_FUNCTIONALITY_URL', trailingslashit( plugins_url( plugin_basename( __DIR__ ) ) ) );
